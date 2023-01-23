32704ef29140b1e45f9ea61b5b4485d193f2b4d7
Radeah <Rmeah2022@elam.co.uk>
2023-01-23 15:59:02 +0000

    added the play but code

--- Assets/Scripts/PlayButton.cs
+++ Assets/Scripts/PlayButton.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayButton : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PlayButton : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
+    public void PlayGame ()
     {
-        
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }

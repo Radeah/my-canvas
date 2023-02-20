f53bce82e00252fa3d1e2a247c5e7ebf00122281
Radeah Meah <rmeah2022@elam.co.uk>
2023-02-20 11:48:28 +0000

    complete credit script

---
+++ Assets/credits.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class credits : MonoBehaviour
+{
+
+    public Transform myCredit;
+    public Vector3 movement;
+
+    void Update()
+    {
+        
+    }
+}
